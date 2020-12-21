65d7aba04b89fb56f23e8778b82a61e043df30ed
Anton Belko <30528165+ko1p@users.noreply.github.com>
2020-12-21 18:03:22 +0300

    Сделал небольшую декомпозицию на бэкенде.

---
+++ backend/soket/soket.js
@@ -0,0 +1,55 @@
+const {getCurrentTime} = require('../utils/getCurrentTime');
+
+module.exports = function logger(http) {
+    const rooms = new Map(); // здесь будем хранить информацию о созданных комнатах и юзерах, которые там сейчас находятся
+
+    const io = require('socket.io')(http, {
+        cors: {
+            origin: "*", // чтобы хром не ругался на CORS
+            methods: ["GET", "POST"],
+            allowedHeaders: ["my-custom-header"],
+            credentials: true
+        }
+    });
+
+    io.on('connection', (socket) => {
+
+        socket.emit('user_connect', socket.id); // сообщаяем клиенту наш soketId
+
+        socket.on('chat_join', ({chatId, name}) => { // при событии chat_join, присоединяемся к комнате, с chatId и name, которую указал клиент
+            socket.join(chatId); // присоединяем его к комнате, с chatId и name, которую указал клиент
+            if (!rooms.has(chatId)) { // добавляем в коллекцию нового юзера либо комнату и юзера если она ещё не была создана
+                rooms.set(
+                    chatId,
+                    new Map([
+                        ['users', new Map().set(socket.id, name)],
+                        ['messages', []],
+                    ]),
+                );
+            } else {
+                rooms.get(chatId).get('users').set(socket.id, name);
+            }
+            const users = [...rooms.get(chatId).get('users').values()]; // формируем список пользователей в конкретной комнате
+            io.to(chatId).emit('user_join', users); // высылаем список клиентов на "фронт"
+        })
+
+        socket.on('chat_message', ({chatId, name, msg}) => { // при событии "chat_mesage"
+            const time = getCurrentTime(); // формирую текущее время сообщения
+            const messageInfo = { // собираю в один объект, дату, текст, имя пользователя
+                msg, time, name
+            };
+            rooms.get(chatId).get('messages').push(messageInfo); // добавляю объект с информацией в "базу данных" на сервере
+            io.to(chatId).emit('chat_message', messageInfo); // высылаю данные в конкретную комнату
+        })
+
+        socket.on('disconnect', () => { // при событии отключения от сокета
+            rooms.forEach((value, chatId) => { // пробежится по всем значениям(каждой комнате), зайдёт в юзерс и удалит его если soket.id совпадет с удаляемым
+                if (value.get('users').delete(socket.id)) {
+                    const users = [...value.get('users').values()]; // сформирует новый список пользователей (без удалённого)
+                    socket.to(chatId).broadcast.emit('user_disconnect', users); // вышлет новый список юзеров на "фронт"
+                }
+            })
+        })
+    })
+
+}
